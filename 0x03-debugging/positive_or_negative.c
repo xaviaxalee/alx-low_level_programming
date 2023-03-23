2e7d416be8bc91e8db66a080a3a299ccd40db530
david effanga <effangasdave@gmail.com>
2023-03-23 04:39:58 -0700

    tottal debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,8 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-#include <stdio.h>
+/**
+ *  positive_or_negative - check if a given number is+ or - or 0
+ *  @i:number to be checked
+ *  return:nothing on sucess
+ */
 
-void positive_or_negative(int i);
-
-#endif /* MAIN_H */
+void  positive_or_negative(int i)
+{
+	if (i>0)
+		printf("%d is positive\n", i);
+	else if(i<0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+}

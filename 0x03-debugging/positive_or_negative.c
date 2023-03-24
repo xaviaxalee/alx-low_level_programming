72e8dcad51e284803e8a4cc22309219e499e6a3c
david effanga <effangasdave@gmail.com>
2023-03-23 22:02:46 -0700

    debugging 01

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,9 +8,9 @@
 
 void  positive_or_negative(int i)
 {
-	if (i>0)
+	if  (i > 0)
 		printf("%d is positive\n", i);
-	else if(i<0)
+	else if (i < 0)
 		printf("%d is negative\n", i);
 	else
 		printf("%d is zero\n", i);
